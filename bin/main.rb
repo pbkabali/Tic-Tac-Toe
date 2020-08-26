4d504d82dab8f2da6744b5d76d4cbcc881880c9a
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-08-26 15:18:34 -0500

    adding review feedback

--- bin/main.rb
+++ bin/main.rb
@@ -14,14 +14,13 @@ puts '-------------'
 puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
 puts '-------------'
 
-# Ask to Player 1 put X
-
+# until board.winner or board.draw
+# take input from player1
 puts 'Player 1, place X'
 player1 = gets.chomp.to_i
+# update board
 board[player1 - 1] = 'X'
-
-# Display the X inside the board in player 1 choice
-
+# show board
 puts 'Your move is displayed on the board'
 puts '-------------'
 puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
@@ -30,15 +29,15 @@ puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
 puts '-------------'
 puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
 puts '-------------'
-
-# Ask to Player 2 put O
-
+# if board.winner
+# break
+# else
+# take input from player2
 puts 'Player 2, place O'
 player2 = gets.chomp.to_i
+# update board
 board[player2 - 1] = 'O'
-
-# Display the X inside the board in player 1 choice
-
+# show the board
 puts 'Your move is displayed on the board'
 puts '-------------'
 puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
@@ -47,9 +46,12 @@ puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
 puts '-------------'
 puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
 puts '-------------'
+# end
+# end
 
-# Repeat user inputs until there is a winner
-
-# Display the winner
-
-puts 'Player _ has won the game'
+# If Board.winner
+# check which player won
+# Announce the winner
+puts 'Winner is ____'
+# Else
+# Announce draw
